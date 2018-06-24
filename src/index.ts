3fe671226b47b58187ebec729d980cc0d4e5fab7
Risabh Kumar <rahulpsd18@gmail.com>
2018-06-24 23:43:29 +0530

    Initial JSON2CSV
    Wrote a function to convert input JSON stream to desired CSV output.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
-import * as AWS from 'aws-sdk';
 import * as fs from 'fs';
+import * as AWS from 'aws-sdk';
+import { Transform, TransformOptions, TransformCallback } from 'stream';
+import { Transform as json2csvStream } from 'json2csv';
 
 const JSONStream = require('JSONStream');
 
 type CognitoISP = AWS.CognitoIdentityServiceProvider
 type ListUsersRequestTypes = AWS.CognitoIdentityServiceProvider.Types.ListUsersRequest
+
+
+class TransformStream extends Transform {
+    constructor(options: TransformOptions) {
+        super(options);
+    }
+
+    _transform(chunk: any, encoding: string, callback: TransformCallback) {
+        for (let entry of chunk) {
+            for (let el in entry.Attributes) {
+                entry[entry.Attributes[el].Name] = entry.Attributes[el].Value;
+            }
+            delete entry.Attributes;
+        }
+        this.push(JSON.stringify(chunk));
+        callback();
+    }
+}
 
 export const backupUsers = async (cognito: CognitoISP, params: ListUsersRequestTypes, file: string) => {
     if (params.UserPoolId == 'all') throw Error('Backing up all pools is not supported yet');
@@ -34,3 +54,15 @@ export const backupUsers = async (cognito: CognitoISP, params: ListUsersRequestT
         })
     }
 }
+
+// Heavily under active development
+// Will be helpful when job creation will be implemented to restore users
+export const json2csv = async (file: string) => {
+    const input = fs.createReadStream(file, { encoding: 'utf8' });
+    const stream: Transform = JSONStream.parse();
+    const transform = new TransformStream({ objectMode: true });
+    const json2csv = new json2csvStream();
+    const output = fs.createWriteStream(`${file}.csv`, { encoding: 'utf8' });
+
+    return input.pipe(stream).pipe(transform).pipe(json2csv);
+};
